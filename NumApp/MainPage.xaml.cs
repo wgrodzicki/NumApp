3a0254369b39dfd89a2650a1abd935d57042b99c
Rosynant <arrobasin@gmail.com>
2024-02-22 19:57:50 +0100

    Add project files.

---
+++ NumApp/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace NumApp;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+
+        this.Window.Height = 470;
+        this.Window.Width = 300;
+
+        this.Window.MinimumHeight = 470;
+        this.Window.MinimumWidth = 300;
+    }
+}
---
+++ NumApp/TODO.cs
@@ -0,0 +1,20 @@
+﻿// TODO
+
+// Front-end:
+// -> one-page, grid-based ✓
+// -> entry/display at the top ✓
+// -> button for operations + numbers below ✓
+
+// Back-end:
+// -> addition
+// -> subtraction
+// -> multiplication
+// -> division
+// -> write operations to JSON
+
+// Nice-to-have:
+// -> random generator
+// -> power
+// -> root
+// -> to hex
+// -> to binary
