297278869d0b2b0c851b4b840085c545642ec633
Rosynant <arrobasin@gmail.com>
2024-02-23 19:53:17 +0100

    Added basic calculations, not working well, though

---
+++ NumApp/CalculatorPage.xaml.cs
@@ -0,0 +1,128 @@
+﻿using NumApp.Helpers;
+
+namespace NumApp;
+
+public partial class CalculatorPage : ContentPage
+{
+    public static double CurrentValue { get; set; }
+    public static string CurrentOperation {  get; set; }
+
+    private bool _moreOptionsShown = false;
+    private List<Button> moreOptionsButtons = new List<Button>();
+
+    public CalculatorPage()
+    {
+        InitializeComponent();
+        CurrentValue = 0;
+        moreOptionsButtons = new List<Button>() { SaveButton, RandomButton, HexButton, BinaryButton };
+    }
+
+    private void ShowMoreOptions(bool shouldShow)
+    {
+        foreach (Button moreOptionsButton in moreOptionsButtons)
+        {
+            if (shouldShow)
+                moreOptionsButton.IsVisible = true;
+            else
+                moreOptionsButton.IsVisible = false;
+        }
+    }
+
+    private void OnMoreButtonClicked(object sender, EventArgs e)
+    {
+        if (!_moreOptionsShown)
+        {
+            this.Window.Height += SaveButton.MinimumHeightRequest;
+            _moreOptionsShown = true;
+            MoreButton.Text = "↑";
+            ShowMoreOptions(true);
+        }
+        else
+        {
+            this.Window.Height -= SaveButton.MinimumHeightRequest;
+            _moreOptionsShown = false;
+            MoreButton.Text = "↓";
+            ShowMoreOptions(false);
+        }
+    }
+
+    private void OnZeroButtonClicked(object sender, EventArgs e)
+    {
+        ButtonActions.ApplyButtonAction(ZeroButton.Text, OperationEntry, OperationLabel, false);
+    }
+
+    private void OnOneButtonClicked(object sender, EventArgs e)
+    {
+        ButtonActions.ApplyButtonAction(OneButton.Text, OperationEntry, OperationLabel, false);
+    }
+
+    private void OnTwoButtonClicked(object sender, EventArgs e)
+    {
+        ButtonActions.ApplyButtonAction(TwoButton.Text, OperationEntry, OperationLabel, false);
+    }
+
+    private void OnThreeButtonClicked(object sender, EventArgs e)
+    {
+        ButtonActions.ApplyButtonAction(ThreeButton.Text, OperationEntry, OperationLabel, false);
+    }
+
+    private void OnFourButtonClicked(object sender, EventArgs e)
+    {
+        ButtonActions.ApplyButtonAction(FourButton.Text, OperationEntry, OperationLabel, false);
+    }
+
+    private void OnFiveButtonClicked(object sender, EventArgs e)
+    {
+        ButtonActions.ApplyButtonAction(FiveButton.Text, OperationEntry, OperationLabel, false);
+    }
+
+    private void OnSixButtonClicked(object sender, EventArgs e)
+    {
+        ButtonActions.ApplyButtonAction(SixButton.Text, OperationEntry, OperationLabel, false);
+    }
+
+    private void OnSevenButtonClicked(object sender, EventArgs e)
+    {
+        ButtonActions.ApplyButtonAction(SevenButton.Text, OperationEntry, OperationLabel, false);
+    }
+
+    private void OnEightButtonClicked(object sender, EventArgs e)
+    {
+        ButtonActions.ApplyButtonAction(EightButton.Text, OperationEntry, OperationLabel, false);
+    }
+
+    private void OnNineButtonClicked(object sender, EventArgs e)
+    {
+        ButtonActions.ApplyButtonAction(NineButton.Text, OperationEntry, OperationLabel, false);
+    }
+
+    private void OnAddButtonClicked(object sender, EventArgs e)
+    {
+        ButtonActions.ApplyButtonAction(AddButton.Text, OperationEntry, OperationLabel, true);
+    }
+
+    private void OnSubtractButtonClicked(object sender, EventArgs e)
+    {
+        ButtonActions.ApplyButtonAction(SubtractButton.Text, OperationEntry, OperationLabel, true);
+    }
+
+    private void OnMultiplyButtonClicked(object sender, EventArgs e)
+    {
+        ButtonActions.ApplyButtonAction(MultiplyButton.Text, OperationEntry, OperationLabel, true);
+    }
+
+    private void OnDivideButtonClicked(object sender, EventArgs e)
+    {
+        ButtonActions.ApplyButtonAction(DivideButton.Text, OperationEntry, OperationLabel, true);
+    }
+
+    private void OnPointButtonClicked(object sender, EventArgs e)
+    {
+        ButtonActions.ApplyButtonAction(PointButton.Text, OperationEntry, OperationLabel, false);
+    }
+
+    private void OnEqualsButtonClicked(object sender, EventArgs e)
+    {
+        ButtonActions.DisplayResult(OperationEntry, OperationLabel);
+    }
+}
--- NumApp/Helpers/ButtonActions.cs
+++ NumApp/Helpers/ButtonActions.cs
@@ -2,11 +2,66 @@
 
 internal class ButtonActions
 {
-    internal static void DisplayButtonAction(Button button, Entry entry, bool isSymbol)
+    internal static void ApplyButtonAction(string buttonText, Entry operationEntry, Label operationLabel, bool isSymbol)
     {
         if (isSymbol)
-            entry.Text += $" {button.Text} ";
+        {
+            if (String.IsNullOrWhiteSpace(operationEntry.Text))
+            {
+                return;
+            }
+
+            double input;
+
+            if (double.TryParse(operationEntry.Text, out input))
+            {
+                CalculatorPage.CurrentOperation = buttonText;
+
+                if (String.IsNullOrEmpty(operationLabel.Text))
+                {
+                    PerformCalculation(buttonText, input);
+                }
+
+                operationLabel.Text += (operationEntry.Text + $" {buttonText} ");
+                operationEntry.Text = "";
+            }
+            else
+            {
+                operationEntry.Text = "";
+            }
+        }
         else
-            entry.Text += $"{button.Text}";
+        {
+            operationEntry.Text += $"{buttonText}";
+        }
+    }
+
+    internal static void DisplayResult(Entry operationEntry, Label operationLabel)
+    {
+        ApplyButtonAction(CalculatorPage.CurrentOperation, operationEntry, operationLabel, true);
+        operationLabel.Text = "";
+        operationEntry.Text = CalculatorPage.CurrentValue.ToString("N2");
+        CalculatorPage.CurrentValue = 0;
+    }
+
+    internal static void PerformCalculation(string operationType, double value)
+    {
+        switch (operationType)
+        {
+            case "+":
+                CalculatorPage.CurrentValue = Operations.Add(CalculatorPage.CurrentValue, value);
+                break;
+            case "-":
+                CalculatorPage.CurrentValue = Operations.Subtract(CalculatorPage.CurrentValue, value);
+                break;
+            case "×":
+                CalculatorPage.CurrentValue = Operations.Multiply(CalculatorPage.CurrentValue, value);
+                break;
+            case "÷":
+                CalculatorPage.CurrentValue = Operations.Divide(CalculatorPage.CurrentValue, value);
+                break;
+            default:
+                break;
+        }
     }
 }
